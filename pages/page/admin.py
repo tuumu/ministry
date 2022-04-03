bdabbebeae639a45327ad88b9f242ffd92e99867
tuumu <102021255+tuumu@users.noreply.github.com>
2022-04-03 23:49:55 +0300

    Delete pages directory

--- pages/page/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Media)
-admin.site.register(Whatusay)
-admin.site.register(Pics_day)
-admin.site.register(Comment)
-admin.site.register(Sermonsay)
