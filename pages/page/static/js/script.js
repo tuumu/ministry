9fbd3b3e2df8d0fe178becb40a3350bb0961f74b
tuumu <102021255+tuumu@users.noreply.github.com>
2022-04-03 19:19:22 +0300

    Add files via upload

---
+++ pages/page/static/js/script.js
@@ -0,0 +1,18 @@
+
+const mobileBtn = document.getElementById('mobile-menu');
+const nav = document.querySelector('nav');
+const mobileExit = document.getElementById('mobile-exit');
+
+mobileBtn.addEventListener('click', () => {
+    nav.classList.add('active');
+
+})
+mobileExit.addEventListener('click', () => {
+    nav.classList.remove('active');
+
+});
+
+         
+        
+  
+    
